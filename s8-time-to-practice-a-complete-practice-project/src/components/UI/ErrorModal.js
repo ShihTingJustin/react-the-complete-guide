6b90afb43e83d876473bb2a38dc4a873e3e5b8a1
Justin Huang <justinhuang777@gmail.com>
2021-07-31 23:13:48 +0800

    97. Managing the Error State

--- s8-time-to-practice-a-complete-practice-project/src/components/UI/ErrorModal.js
+++ s8-time-to-practice-a-complete-practice-project/src/components/UI/ErrorModal.js
@@ -4,10 +4,10 @@ import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
-const ErrorModal = ({ title, message }) => {
+const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      <div className={classes.backdrop} />
+      <div className={classes.backdrop} onClick={onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{title}</h2>
@@ -16,7 +16,7 @@ const ErrorModal = ({ title, message }) => {
           <p>{message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>Okay</Button>
+          <Button onClick={onConfirm}>Okay</Button>
         </footer>
       </Card>
     </>
--- s8-time-to-practice-a-complete-practice-project/src/components/Users/AddUser.js
+++ s8-time-to-practice-a-complete-practice-project/src/components/Users/AddUser.js
@@ -8,13 +8,22 @@ import classes from "./AddUser.module.css";
 const AddUser = ({ onAddUser }) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
 
   const addUserHandler = (e) => {
     e.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     onAddUser(enteredUsername, enteredAge);
@@ -30,9 +39,19 @@ const AddUser = ({ onAddUser }) => {
     setEnteredAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
     <>
-      <ErrorModal title="An error occured" message="Something went wrong" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="userName">Username</label>
