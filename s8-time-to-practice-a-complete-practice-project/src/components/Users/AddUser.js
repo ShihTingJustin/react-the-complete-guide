9a9812eafc7b00f85554243b3c350b9c92f341c4
Justin Huang <justinhuang777@gmail.com>
2021-07-31 19:17:49 +0800

    93. Adding Validation & Resetting Logic

--- s8-time-to-practice-a-complete-practice-project/src/components/Users/AddUser.js
+++ s8-time-to-practice-a-complete-practice-project/src/components/Users/AddUser.js
@@ -1,33 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import classes from "./AddUser.module.css"
+import classes from "./AddUser.module.css";
 
 const AddUser = () => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
-  
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
-    console.log(enteredUsername, enteredAge)
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
-  const usernameChangeHandler = e => {
+  const usernameChangeHandler = (e) => {
     setEnteredUsername(e.target.value);
-  }
+  };
 
-  const ageChangeHandler =e => {
+  const ageChangeHandler = (e) => {
     setEnteredAge(e.target.value);
-  }
+  };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="userName">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">age</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
