9741e2e3e71346716db638a07a6af19d9c940524
Justin Huang <justinhuang777@gmail.com>
2021-08-08 18:04:19 +0800

    124. Building & Using a Custom Context Provider Component

--- s10-Advanced-Handling-Side-Effects-Using-Reducers-and-Using-the-Context-API/src/store/auth-context.js
+++ s10-Advanced-Handling-Side-Effects-Using-Reducers-and-Using-the-Context-API/src/store/auth-context.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoginInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoginInfo === "1") setIsLoggedIn(true);
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
