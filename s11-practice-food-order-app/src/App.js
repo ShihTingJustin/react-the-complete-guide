1dd037bbd6586ac94021ccdf15d3624c5d119142
ShihTingJustin <justinhuang777@gmail.com>
2021-08-15 00:02:23 +0800

    134. Adding a "Meals" Component

--- s11-practice-food-order-app/src/App.js
+++ s11-practice-food-order-app/src/App.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from "react";
 
+// components
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
