81305907506dd72aaa661bf5cd0b634b5618c8d4
ShihTingJustin <justinhuang777@gmail.com>
2021-08-15 21:34:42 +0800

    142. Using the Context

--- s11-practice-food-order-app/src/components/Layout/HeaderCartButton.js
+++ s11-practice-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
+
 // css module
 import classes from "./HeaderCartButton.module.css";
 
@@ -5,13 +9,19 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+
+  const nubmerOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{nubmerOfCartItems}</span>
     </button>
   );
 };
