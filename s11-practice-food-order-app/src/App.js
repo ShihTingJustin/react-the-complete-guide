eb3fed4239fda836c92e1c99c59e0ab623f50bb5
ShihTingJustin <justinhuang777@gmail.com>
2021-08-15 21:28:51 +0800

    141. Adding a Cart Context

--- s11-practice-food-order-app/src/App.js
+++ s11-practice-food-order-app/src/App.js
@@ -1,9 +1,10 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
 // components
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -17,13 +18,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShownCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
