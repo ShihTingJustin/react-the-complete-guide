0a324020e2cd68ecc9aa240da5b5b47f8ab58fc9
Justin Huang <justinhuang777@gmail.com>
2021-08-23 22:42:16 +0800

    152. Component Updates In Action

--- s12-a-look-behind-the-scenes-of-react-and-optimization-techniques/src/App.js
+++ s12-a-look-behind-the-scenes-of-react-and-optimization-techniques/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button"
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING')
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph)
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
